c7da46cf7a80a41412fa65e463c6d42d3d84aecb
Angel Georgiev <angelgeor0560@gmail.com>
2023-06-06 14:48:22 +0300

    Add files via upload

---
+++ NestedLoopsExercise/demo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    int numberOne = Integer.parseInt(scanner.nextLine());
+    int numberTwo = Integer.parseInt(scanner.nextLine());
+
+        for (int i = numberOne; i <= numberTwo ; i++) {
+            String currentNumber = "" + i;
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int j = 0; j < currentNumber.length(); j++) {
+
+
+
+            }
+        }
+
+    }
+
+}
