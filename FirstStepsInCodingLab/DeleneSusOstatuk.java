184ec930db0b8d4da35e7693d0817972df9c6faf
Angel Georgiev <angelgeor0560@gmail.com>
2023-06-06 13:21:50 +0300

    Delete FirstStepsInCodingLab directory

--- FirstStepsInCodingLab/DeleneSusOstatuk.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class DeleneSusOstatuk {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println(7 % 2);
-
-    }
-}
